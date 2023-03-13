2196e7b1fa6b463a16955016d0e463c06ff653ed
Sem Sinchenko <semyonsinchenko@yahoo.com>
2023-03-13 17:31:47 +0100

    Add HadoopFileSystem draft
    Currently implemented:
    - read_utf8
    - write_utf8
    - glob
    
    The reason of such a class is to help people to read and write some config files
    from Distributed FS (HDFS, DBFS, S3) with one unified interface. Under the hood
    it uses `py4j` and `org.apache.hadoop.fs.FileSystem.get(uri, config)` to choose
    right implementation of `FileSystem` (possible options are S3, Hdfs, Local FS
    and maybe other).
    
    + test
    
     On branch feature-hadoop-fs
     Changes to be committed:
    	new file:   eren/fs.py
    	new file:   tests/test_fs.py

---
+++ tests/test_fs.py
@@ -0,0 +1,60 @@
+import os
+
+from delta import configure_spark_with_delta_pip
+from pyspark.sql import SparkSession
+
+from eren import fs
+
+builder = (
+    SparkSession.builder.appName("MyApp")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
+    .config("spark.sql.shuffle.partitions", "2")
+)
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+
+def test_read(tmp_path):
+    with open(os.path.join(tmp_path, "test_read.txt"), "w") as f_:
+        f_.write("testing_string")
+
+    file_system = fs.HadoopFileSystem(spark, "file:///")
+    read_result = file_system.read_utf8(os.path.join(tmp_path, "test_read.txt"))
+
+    assert read_result == "testing_string"
+
+
+def test_read_multiline(tmp_path):
+    with open(os.path.join(tmp_path, "test_read_mult.txt"), "w") as f_:
+        f_.write("first_line\nsecond_line")
+
+    file_system = fs.HadoopFileSystem(spark, "file:///")
+    read_result = file_system.read_utf8(os.path.join(tmp_path, "test_read_mult.txt"))
+    lines = read_result.split("\n")
+
+    assert len(lines) == 2
+    assert lines[0] == "first_line"
+    assert lines[1] == "second_line"
+
+
+def test_write(tmp_path):
+    file_system = fs.HadoopFileSystem(spark, "file:///")
+    file_system.write_utf8(
+        os.path.join(tmp_path, "test_write.txt"), "testing_string", "w"
+    )
+
+    with open(os.path.join(tmp_path, "test_write.txt"), "r") as f_:
+        res = f_.read()
+
+    assert res == "testing_string"
+
+
+def test_glob():
+    file_system = fs.HadoopFileSystem(spark, "file:///")
+    list_of_files = file_system.glob(os.path.join(".", "*"))
+    assert len(list_of_files) > 0
+    assert [f for f in list_of_files if f.name == "tests"][0].is_dir
